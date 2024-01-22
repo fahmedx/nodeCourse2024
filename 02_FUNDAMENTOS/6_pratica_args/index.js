ab12d3e17b3e3407d99329b0f5196e1e6b58dd76
fahmedx <fahmecanaldx@gmail.com>
2024-01-21 22:36:10 -0300

    Progresso nos estudos

---
+++ 02_FUNDAMENTOS/6_pratica_args/index.js
@@ -0,0 +1,9 @@
+const minimist = require("minimist")
+const soma = require("./soma").soma
+
+const args = minimist(process.argv.slice(2))
+
+const a = args["a"]
+const b = args ["b"]
+
+soma(a,b)
---
+++ 02_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,14 @@
+const x = 1
+const y = 'Texto'
+const z = [1, 2]
+
+console.log(x,y,z)
+
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+
+console.log(`%s`,y)
+
+setTimeout(()=>{
+    console.clear()
+}, 2000)
