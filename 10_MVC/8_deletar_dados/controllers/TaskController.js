93a8b4690314e2781ab232de1dda9a3c63afb4c4
fahmedx <fahmecanaldx@gmail.com>
2024-02-13 19:00:55 -0300

    Consulta delete e edicao de dados com mvc

--- 10_MVC/8_deletar_dados/controllers/TaskController.js
+++ 10_MVC/8_deletar_dados/controllers/TaskController.js
@@ -17,6 +17,12 @@ module.exports = class TaskController {
         res.redirect('/tasks')
     }
 
+    static async removeTask(req,res){
+        const id = req.body.id
+        await Task.destroy({where: {id: id}})
+        res.redirect('/tasks')
+    }
+
     static async showTasks(req, res){
         const tasks = await Task.findAll({raw: true})
         res.render('tasks/all', {tasks})
