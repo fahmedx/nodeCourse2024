fb8f6e84098873681c24d1022ee607a76a9627b5
fahmedx <fahmecanaldx@gmail.com>
2024-02-18 20:27:45 -0300

    Continuação do projeto, falta só dashboard

--- 11_Thoughts/routes/toughtsRoutes.js
+++ 11_Thoughts/routes/toughtsRoutes.js
@@ -2,6 +2,15 @@ const express = require('express')
 const router = express.Router()
 const toughtController = require('../controllers/toughtController')
 
+
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/add', checkAuth, toughtController.createTought)
+router.post('/add', checkAuth, toughtController.createToughtSave)
+router.get('/edit/:id', checkAuth, toughtController.updateTought)
+router.post('/edit', checkAuth, toughtController.updateToughtSave)
+router.get('/dashboard', checkAuth, toughtController.dashboard)
+router.post('/remove', checkAuth, toughtController.removeTought)
 router.get('/', toughtController.showToughts)
 
 module.exports = router
