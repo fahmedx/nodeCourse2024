33f75aee9101695de7d3b3920fe3a46e60e76f3b
fahmedx <fahmecanaldx@gmail.com>
2024-02-27 00:15:59 -0300

    Adicionando produtos mongoDB

--- 12_MONGO_NODE/4_retornando_dados/controller/ProductController.js
+++ 12_MONGO_NODE/4_retornando_dados/controller/ProductController.js
@@ -1,8 +1,11 @@
  const Product = require('../models/Product')
 
  module.exports = class ProductController{
-    static showProducts(req,res){
-        res.render('products/all')
+
+    static async showProducts(req,res){
+        const products = await Product.getProducts()
+
+        res.render('products/all', {products})
     }
 
     static createProduct(req,res){
@@ -12,9 +15,10 @@
     static createProductsPost(req,res){
         const name = req.body.name
         const price = req.body.price
+        const image = req.body.image
         const description = req.body.description
 
-        const product = new Product(name, price, description)
+        const product = new Product(name, image, price, description)
 
         product.save()
 
