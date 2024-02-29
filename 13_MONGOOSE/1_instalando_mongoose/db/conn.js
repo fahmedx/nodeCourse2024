9719f6e0b5116cc992fe34f41f520211d914ecfe
fahmedx <fahmecanaldx@gmail.com>
2024-02-28 23:37:37 -0300

    Conexao com mongoose criada

--- 13_MONGOOSE/1_instalando_mongoose/db/conn.js
+++ 13_MONGOOSE/1_instalando_mongoose/db/conn.js
@@ -1,18 +1,8 @@
-const {MongoClient} = require("mongodb")
+const mongoose = require('mongoose')
 
-const uri = "mongodb://127.0.0.1:27017/primeirobanco"
-
-const client = new MongoClient(uri)
-
-async function run(){
-    try {
-        await client.connect()
-        console.log('Conectando ao mongodb')
-    } catch (err) {
-        console.log(err)
-    }
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/mongoose')
+    console.log('Conectou ao mongoDB via Mongoose!')
 }
 
-run()
-
-module.exports = client
+main().exports = mongoose
