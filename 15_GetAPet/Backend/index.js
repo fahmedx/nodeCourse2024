4ded73971edacd2205cd9875c33e46145780a563
fahmedx <fahmecanaldx@gmail.com>
2024-03-07 00:00:06 -0300

    introdução a apis e inicio do Get a Pet

---
+++ 15_GetAPet/Backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 5000
+
+// Config JSON Response
+app.use(express.json())
+
+
+// Solve CORS
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+
+// Public folder images
+app.use(express.static('public'))
+
+// Routes
+
+app.get('/', (req,res)=>{
+    res.status(200).json({message: 'Primeira rota criada com sucesso'})
+})
+
+console.log(`Server started at ${port} port` )
+
+app.listen(port)
